a4ce9b29c8baf925f16e5907fd8ef1a0e871ac98
Paul Baxter <jareth_2005@yahoo.com>
2026-07-29 12:56:50 -0400

    Add files via upload

--- RegexEngine.h
+++ RegexEngine.h
@@ -263,6 +263,24 @@ private:
 			if (e == 'd') return makeRange({ {'0', '9'} });
 			if (e == 'w') return makeRange({ {'a', 'z'}, {'A', 'Z'}, {'0', '9'}, {'_', '_'} });
 			if (e == 's') return makeRange({ {' ', ' '}, {'\t', '\t'}, {'\r', '\r'}, {'\n', '\n'} });
+			if (e == 'D') return makeRange({ 
+			        {0, static_cast<unsigned_char>('0'-1)},
+			        {static_cast<unsigned_char>('9'+1), static_cast<unsigned_char>(255)}
+			});
+			if (e == 'W') return makeRange({
+			        {0, static_cast<unsigned_char>('0'-1)},
+			        {static_cast<unsigned_char>('9'+1), static_cast<unsigned_char>('A'-1)},
+			        {static_cast<unsigned_char>('Z'+1), static_cast<unsigned_char>('_'-1)},
+			        {static_cast<unsigned_char>('_'+1), static_cast<unsigned_char>('a'-1)},
+			        {static_cast<unsigned_char>('z'+1), static_cast<unsigned_char>(255)}
+			});
+			if (e == 'S') return makeRange({ 
+			        {0, static_cast<unsigned_char>('\t'-1)},
+			        {static_cast<unsigned_char>('\n'+1), static_cast<unsigned_char>('\r'-1)},
+			        {static_cast<unsigned_char>('\r'+1), static_cast<unsigned_char>(' '-1)},
+			        {static_cast<unsigned_char>(' '+1), static_cast<unsigned_char>(255)}
+			});
+
 			char r = e;
 			if (e == 'n') r = '\n';
 			else if (e == 'r') r = '\r';
